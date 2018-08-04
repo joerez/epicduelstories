36ba81900f5a7195c2ee3e9b250d5c09e9b8147d
Joerez <rezenbiz@gmail.com>
2018-08-04 13:45:00 -0700

    player add

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express();
 const User = require('../models/user');
+const Player = require('../models/player');
 
 
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
       let currentUser;
+
+      Player.find({pending : true}).then((players) => {
+
+
       if (req.user) {
         User.findById(req.user._id, (err, user) => {
-          res.render('index/index', {currentUser: user});
+          res.render('index/index', {currentUser: user, players});
         })
       } else {
-      res.render('index/index');
+      res.render('index/index', { players });
       }
     })
-
+  })
 };
