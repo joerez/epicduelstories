12ead408058e3eb54b01657b9f6545250318c613
Joerez <rezenbiz@gmail.com>
2018-08-05 00:20:57 -0700

    embellishment

--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,1 @@
 console.log('console');
-
-const revealer = document.getElementById('revealer');
-const popHide = document.getElementById('pop-hide');
-
-const story = document.getElementById('story-pop');
-
-revealer.addEventListener("click", function() {
-  return  story.style.display = 'block';
-})
-
-popHide.addEventListener("click", function() {
-  story.style.display = 'none';
-})
